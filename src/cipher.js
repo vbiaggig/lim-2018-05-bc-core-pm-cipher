77fd181b83169ceaa1bcec324a8b165e87233618
vbiaggi10 <vbiaggi10@gmail.com>
2018-05-23 18:16:45 -0500

    js actualizacion2

--- src/cipher.js
+++ src/cipher.js
@@ -1,2 +1,11 @@
 window.cipher = {
+    /* */
+};
+
+function buttonEncode(){
+    const cipher_message = document.getElementById("cipher_message").value;
+    const shift = parseInt(document.getElementById("shift").value);
+    const cipher_text = document.getElementById("cipher_text");
+
+    
 }
