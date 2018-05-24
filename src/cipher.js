069c03d44ef814909f39ef175914f57417b73a4a
Valeria <vbiaggi10@gmail.com>
2018-05-23 23:33:42 -0500

    html + css + js (basico) terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,31 @@
 window.cipher = {
-    /* */
+    encode: function(string, offset){
+        return cipherEncode(string,offset);
+    },
+    decode: function(string, offset){
+        return cipherDecode(string,offset);
+    },
 };
 
-function buttonEncode(){
-    const cipher_message = document.getElementById("cipher_message").value;
-    const shift = parseInt(document.getElementById("shift").value);
-    const cipher_text = document.getElementById("cipher_text");
-
+function cipherEncode(string, offset) {
+    let ciphertext = '';
     
+    for(let i=0 ; i < string.length ; i++){
+        let cAscii = string.charCodeAt(i);
+        let letter;
+        if((cAscii >= 65) && (cAscii <= 90)){
+            letter = (cAscii-65+offset)%26+65;
+        }else if((cAscii >= 97) && (cAscii <= 122)){
+            letter = (cAscii-97+offset)%26+97;
+        }
+        ciphertext += String.fromCharCode(letter);
+    }
+    
+    return ciphertext;
+}
+function cipherDecode(string,offset) {
+    return cipherEncode(string, 26-offset)
+}
+function buttonEncode(){
+    cipher.encode()
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-function cipherEncode(string, offset) {
-    const holatext = "";
-    for(const i = 0; i < string.length; i++) {
-        const plainCharacter = string.charCodeAt(i);
-        if(plainCharacter >= 97 && plainCharacter <= 122) {
-            holatext += String.fromCharCode((plainCharacter - 97 + offset) % 26 + 97);
-        } else if(plainCharacter >= 65 && plainCharacter <= 90) {
-            holatext += String.fromCharCode((plainCharacter - 65 + offset) % 26 + 65);
-        } else {
-            holatext += String.fromCharCode(plainCharacter);
-        }
-    }
-    return text;
-}
-
 function buttonEncode(){
     const cipher_message = document.getElementById("cipher_message").value;
-    const number = parseInt(document.getElementById("number").value);
-    const cipher_text = document.getElementById("cipher_text").value;
+    const shift = parseInt(document.getElementById("shift").value);
+    const cipher_text = document.getElementById("cipher_text");
+    cipher_text.innerHTML = cipher.encode(cipher_message,shift)
+}
 
-    console.log(alert(cipherEncode(cipher_message, number)));
+function buttonDecode(){
+    const cipher_message = document.getElementById("cipher_message").value;
+    const shift = parseInt(document.getElementById("shift").value);
+    const cipher_text = document.getElementById("cipher_text");
+    cipher_text.innerHTML = cipher.decode(cipher_message,shift)
 }
