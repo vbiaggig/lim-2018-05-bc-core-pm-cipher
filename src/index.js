0de3b4c340be1a8c77de716060079b826fc02974
Valeria <vbiaggi10@gmail.com>
2018-05-25 09:07:06 -0500

    boton copiar

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-const buttonEncode = document.getElementById("buttonEncode");
-const buttonDecode = document.getElementById("buttonDecode");
-
 document.getElementById("button").addEventListener("click", () => {
     document.getElementById("cipher_area").style.display = "";
     document.getElementById("container").style.display = "none";
 });
+
 document.getElementById("principal").addEventListener("click", () => {
     document.getElementById("cipher_area").style.display = "none";
     document.getElementById("container").style.display = "";
 });
 
-buttonEncode.addEventListener("click", ()=>{
+document.getElementById("buttonCopyText").addEventListener("click", ()=>{
+    var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById("cipher_text").innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+})
+
+document.getElementById("buttonEncode").addEventListener("click", ()=>{
     const cipher_message = document.getElementById("cipher_message").value;
     const shift = parseInt(document.getElementById("shift").value);
     const cipher_text = document.getElementById("cipher_text");
     cipher_text.innerHTML = cipher.encode(cipher_message,shift);
 });
-buttonDecode.addEventListener("click", ()=>{
+
+document.getElementById("buttonDecode").addEventListener("click", ()=>{
     const cipher_message = document.getElementById("cipher_message").value;
     const shift = parseInt(document.getElementById("shift").value);
     const cipher_text = document.getElementById("cipher_text");
