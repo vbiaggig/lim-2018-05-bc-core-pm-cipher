56e382111b315b819b34e00355f5551aff009249
vbiaggi10 <vbiaggi10@gmail.com>
2018-05-28 17:13:11 -0500

    responsive 2

--- src/cipher.js
+++ src/cipher.js
@@ -1,42 +1,41 @@
 window.cipher = {
-    encode: (offset, string) => {
-      let ciphertext = '';
-      offset = offset % 26;
-      for(let i=0 ; i < string.length ; i++) {
-          let cAscii = string.charCodeAt(i);
-          let letter;
-          if((cAscii >= 65) && (cAscii <= 90)){
-              letter = (cAscii-65+offset)%26+65;
-          }else if((cAscii >= 97) && (cAscii <= 122)){
-              letter = (cAscii-97+offset)%26+97;
-          }else {
-              letter = cAscii;
-          }
-          ciphertext += String.fromCharCode(letter);
+  encode: (offset, string) => {
+    offset = parseInt(offset);
+    let ciphertext = '';
+    offset = offset % 26;
+    for(let i=0 ; i < string.length ; i++) {
+      let cAscii = string.charCodeAt(i);
+      let letter;
+      if((cAscii >= 65) && (cAscii <= 90)){
+        letter = (cAscii-65+offset)%26+65;
+      }else if((cAscii >= 97) && (cAscii <= 122)){
+        letter = (cAscii-97+offset)%26+97;
+      }else {
+        letter = cAscii;
       }
-      
-      return ciphertext;
-    },
-    decode: (offset, string) => {
-        let ciphertext = '';
-        offset = offset % 26;
-        for(let i=0 ; i < string.length ; i++) {
-            let cAscii = string.charCodeAt(i);
-            let letter;
-            if((cAscii >= 65) && (cAscii <= 90)){
-                letter = (cAscii-65-offset+26)%26+65;
-            }else if ((cAscii >= 97) && (cAscii <= 122)) {
-                letter = (cAscii-97-offset+26)%26+97;
-            }else {
-                letter=cAscii;
-            }
-            ciphertext += String.fromCharCode(letter);
-        }
-        
-        return ciphertext;
-    },
-    createCipherWithOffset: () => {
-        /* */
+        ciphertext += String.fromCharCode(letter);
     }
-  };
+    return ciphertext;
+  },
+  decode: (offset, string) => {
+    let ciphertext = '';
+    offset = offset % 26;
+    for(let i=0 ; i < string.length ; i++) {
+      let cAscii = string.charCodeAt(i);
+      let letter;
+      if((cAscii >= 65) && (cAscii <= 90)){
+        letter = (cAscii-65-offset+26)%26+65;
+      }else if ((cAscii >= 97) && (cAscii <= 122)) {
+        letter = (cAscii-97-offset+26)%26+97;
+      }else {
+        letter=cAscii;
+      }
+      ciphertext += String.fromCharCode(letter);
+    }
+    return ciphertext;
+  },  
+  createCipherWithOffset: () => {
+     /* */
+  }
+};
   
