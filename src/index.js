f88eb5b46bb4a77effc30a934b221f22827ab7b0
Valeria <vbiaggi10@gmail.com>
2018-06-01 11:26:40 -0500

    Comentando

--- src/index.js
+++ src/index.js
@@ -1,46 +1,69 @@
-document.getElementById("button").addEventListener("click", () => {
-    document.getElementById("cipher_area").style.display = "";
-    document.getElementById("cipher_information").style.display = "none";
+//Boton para desplegar cipherArea y ocultar cipherInformation
+document.getElementById('buttonInformation').addEventListener('click', () => {
+    //Desplegando cipherArea
+    document.getElementById('cipherArea').style.display = '';
+    //Ocultando cipherInformation
+    document.getElementById('cipherInformation').style.display = 'none';
 });
 
-document.getElementById("principal").addEventListener("click", () => {
-    document.getElementById("cipher_area").style.display = "none";
-    document.getElementById("cipher_steps").style.display = "none";
-    document.getElementById("cipher_information").style.display = "";
+//Boton para volver al incio de la pagina
+document.getElementById('logoLink').addEventListener('click', () => {
+    //Ocultando cipherArea
+    document.getElementById('cipherArea').style.display = 'none';
+    //Ocultando cipherSteps
+    document.getElementById('cipherSteps').style.display = 'none';
+    //Desplegando cipherInformation
+    document.getElementById('cipherInformation').style.display = '';
 });
 
-document.getElementById("buttonCopyText").addEventListener("click", ()=>{
-    var aux = document.createElement("input");
-    aux.setAttribute("value", document.getElementById("cipher_text").innerHTML);
+//Boton de copiar texto descifrado o cifrado en el portapapeles
+document.getElementById('buttonCopyText').addEventListener('click', ()=>{
+    //auxiliar que almacena un elemento parrafo creado
+    var aux = document.createElement('input');
+    //dandole un atributo al auxiliar que establece un nombre y un valor
+    aux.setAttribute('value', document.getElementById('cipherText').innerHTML);
+    //agregando el elemento parrafo al final del cuerpo
     document.body.appendChild(aux);
+    //seleccionando el contenido del auxiliar.
     aux.select();
-    document.execCommand("copy");
+    //ejecutando comandos para copiar el contenido en el portapapeles
+    document.execCommand('copy');
+    //removiendo el auxiliar
     document.body.removeChild(aux);
 })
 
-document.getElementById("buttonOpenSteps").addEventListener("click", () => {
-    document.getElementById("cipher_steps").style.display = "";
-    var elmnt = document.getElementById("cipher_steps");
+//Boton para abrir las instrucciones
+document.getElementById('buttonOpenSteps').addEventListener('click', () => {
+    document.getElementById('cipherSteps').style.display = '';
+    //Dandole scroll a donde abre el div
+    var elmnt = document.getElementById('scroll');
     elmnt.scrollIntoView();
     //window.scroll(0,1200);
 });
-/*document.getElementById("buttonOpenSteps").addEventListener("click", () => {
-    document.getElementById("cipher_steps").style.display = "";
-});*/
-document.getElementById("buttonCloseSteps").addEventListener("click", () => {
-    document.getElementById("cipher_steps").style.display = "none";
+
+//Boton para cerrar las instrucciones
+document.getElementById('buttonCloseSteps').addEventListener('click', () => {
+    document.getElementById('cipherSteps').style.display = 'none';
 });
 
-document.getElementById("buttonEncode").addEventListener("click", ()=>{
-    const cipher_message = document.getElementById("cipher_message").value;
-    const shift = parseInt(document.getElementById("shift").value);
-    const cipher_text = document.getElementById("cipher_text");
-    cipher_text.innerHTML = cipher.encode(shift, cipher_message);
+//Boton para realizar la funcion cifrar
+document.getElementById('buttonEncode').addEventListener('click', ()=>{
+    //almacenando elementos de area de texto del mensaje escrito
+    const cipherMessage = document.getElementById('cipherMessage').value;
+    //almacenando posicion
+    const cipherNumber = parseInt(document.getElementById('cipherNumber').value);
+    //mostrando mensaje cifrado
+    const cipherText = document.getElementById('cipherText');
+    cipherText.innerHTML = cipher.encode(cipherNumber, cipherMessage);
 });
 
-document.getElementById("buttonDecode").addEventListener("click", ()=>{
-    const cipher_message = document.getElementById("cipher_message").value;
-    const shift = parseInt(document.getElementById("shift").value);
-    const cipher_text = document.getElementById("cipher_text");
-    cipher_text.innerHTML = cipher.decode(shift, cipher_message);
+//Boton para realizar la funcion descifrar 
+document.getElementById('buttonDecode').addEventListener('click', ()=>{
+    //almacenando elementos de area de texto del mensaje escrito
+    const cipherMessage = document.getElementById('cipherMessage').value;
+    //almacenando posicion
+    const cipherNumber = parseInt(document.getElementById('cipherNumber').value);
+    //mostrando mensaje descifrado
+    const cipherText = document.getElementById('cipherText');
+    cipherText.innerHTML = cipher.decode(cipherNumber, cipherMessage);
 });
