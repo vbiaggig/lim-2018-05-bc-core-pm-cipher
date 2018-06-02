10201c1fb518dd364c72814ec3194a6a911d8893
Valeria <vbiaggi10@gmail.com>
2018-06-02 17:20:18 -0500

    Identacion

--- src/index.js
+++ src/index.js
@@ -1,69 +1,69 @@
-//Boton para desplegar cipherArea y ocultar cipherInformation
+// Boton para desplegar cipherArea y ocultar cipherInformation
 document.getElementById('buttonInformation').addEventListener('click', () => {
-    //Desplegando cipherArea
+    // Desplegando cipherArea
     document.getElementById('cipherArea').style.display = '';
-    //Ocultando cipherInformation
+    // Ocultando cipherInformation
     document.getElementById('cipherInformation').style.display = 'none';
 });
 
-//Boton para volver al incio de la pagina
+// Boton para volver al incio de la pagina
 document.getElementById('logoLink').addEventListener('click', () => {
-    //Ocultando cipherArea
+    // Ocultando cipherArea
     document.getElementById('cipherArea').style.display = 'none';
-    //Ocultando cipherSteps
+    // Ocultando cipherSteps
     document.getElementById('cipherSteps').style.display = 'none';
-    //Desplegando cipherInformation
+    // Desplegando cipherInformation
     document.getElementById('cipherInformation').style.display = '';
 });
 
-//Boton de copiar texto descifrado o cifrado en el portapapeles
+// Boton de copiar texto descifrado o cifrado en el portapapeles
 document.getElementById('buttonCopyText').addEventListener('click', ()=>{
-    //auxiliar que almacena un elemento parrafo creado
-    var aux = document.createElement('input');
-    //dandole un atributo al auxiliar que establece un nombre y un valor
+    // auxiliar que almacena un elemento parrafo creado
+    let aux = document.createElement('input');
+    // dandole un atributo al auxiliar que establece un nombre y un valor
     aux.setAttribute('value', document.getElementById('cipherText').innerHTML);
-    //agregando el elemento parrafo al final del cuerpo
+    // agregando el elemento parrafo al final del cuerpo
     document.body.appendChild(aux);
-    //seleccionando el contenido del auxiliar.
+    // seleccionando el contenido del auxiliar.
     aux.select();
-    //ejecutando comandos para copiar el contenido en el portapapeles
+    // ejecutando comandos para copiar el contenido en el portapapeles
     document.execCommand('copy');
-    //removiendo el auxiliar
+    // removiendo el auxiliar
     document.body.removeChild(aux);
 })
 
-//Boton para abrir las instrucciones
+// Boton para abrir las instrucciones
 document.getElementById('buttonOpenSteps').addEventListener('click', () => {
     document.getElementById('cipherSteps').style.display = '';
-    //Dandole scroll a donde abre el div
-    var elmnt = document.getElementById('scroll');
+    // Dandole scroll a donde abre el div
+    let elmnt = document.getElementById('scroll');
     elmnt.scrollIntoView();
-    //window.scroll(0,1200);
+    // window.scroll(0,1200);
 });
 
-//Boton para cerrar las instrucciones
+// Boton para cerrar las instrucciones
 document.getElementById('buttonCloseSteps').addEventListener('click', () => {
     document.getElementById('cipherSteps').style.display = 'none';
 });
 
-//Boton para realizar la funcion cifrar
+// Boton para realizar la funcion cifrar
 document.getElementById('buttonEncode').addEventListener('click', ()=>{
-    //almacenando elementos de area de texto del mensaje escrito
+    // almacenando elementos de area de texto del mensaje escrito
     const cipherMessage = document.getElementById('cipherMessage').value;
-    //almacenando posicion
+    // almacenando posicion
     const cipherNumber = parseInt(document.getElementById('cipherNumber').value);
-    //mostrando mensaje cifrado
+    // mostrando mensaje cifrado
     const cipherText = document.getElementById('cipherText');
     cipherText.innerHTML = cipher.encode(cipherNumber, cipherMessage);
 });
 
-//Boton para realizar la funcion descifrar 
+// Boton para realizar la funcion descifrar 
 document.getElementById('buttonDecode').addEventListener('click', ()=>{
-    //almacenando elementos de area de texto del mensaje escrito
+    // almacenando elementos de area de texto del mensaje escrito
     const cipherMessage = document.getElementById('cipherMessage').value;
-    //almacenando posicion
+    // almacenando posicion
     const cipherNumber = parseInt(document.getElementById('cipherNumber').value);
-    //mostrando mensaje descifrado
+    // mostrando mensaje descifrado
     const cipherText = document.getElementById('cipherText');
     cipherText.innerHTML = cipher.decode(cipherNumber, cipherMessage);
 });
